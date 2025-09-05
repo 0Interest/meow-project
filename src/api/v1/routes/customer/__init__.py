77899672c88305a2ebc4738bf29c28999331bf58
ItayBe <itaybe777@gmail.com>
2025-09-05 05:16:40 +0300

    fix: router imports and usage of employees in the customers router

--- src/api/v1/routes/customer/__init__.py
+++ src/api/v1/routes/customer/__init__.py
@@ -1,2 +1,1 @@
-from src.api.v1.routes.employee.main import router as employee_router
 from src.api.v1.routes.customer.main import router as customer_router
--- src/api/v1/routes/customer/main.py
+++ src/api/v1/routes/customer/main.py
@@ -12,27 +12,26 @@ router = APIRouter(prefix="/customers", tags=["Customers"])
 
 @router.post("/", response_model=CustomerResponse)
 def create_customer(customer: CustomerCreate, db: Session = Depends(get_db)):
-    new_employee = Customer(
-        # TODO: change to customer_first_name/last_name
-        employee_first_name=customer.customer_first_name,
-        employee_last_name=customer.customer_last_name,
+    new_customer = Customer(
+        customer_first_name=customer.customer_first_name,
+        customer_last_name=customer.customer_last_name,
     )
-    db.add(new_employee)
+    db.add(new_customer)
     db.commit()
-    db.refresh(new_employee)
-    return new_employee
+    db.refresh(new_customer)
+    return new_customer
 
 
 @router.get("/{customer_id}", response_model=CustomerResponse)
-def get_employee(customer_id: UUID, db: Session = Depends(get_db)):
+def get_customer(customer_id: UUID, db: Session = Depends(get_db)):
     emp = db.query(Customer).filter(Customer.customer_id == customer_id).first()
     if not emp:
         raise HTTPException(status_code=404, detail="Customer not found")
     return emp
 
 
-@router.delete("/{employee_id}", response_model=CustomerResponse)
-def delete_employee(customer_id: UUID, db: Session = Depends(get_db)):
+@router.delete("/{customer_id}", response_model=CustomerResponse)
+def delete_customer(customer_id: UUID, db: Session = Depends(get_db)):
     emp = db.query(Customer).filter(Customer.customer_id == customer_id).first()
     if not emp:
         raise HTTPException(status_code=404, detail="Customer not found")
