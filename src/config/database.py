361eb615ef2d5aa652de3c650ea4dbe30de7ea60
ItayBe <itaybe777@gmail.com>
2025-09-06 05:01:12 +0300

    feat: add requiremnets.txt and move .env to the parent directory

--- src/config/database.py
+++ src/config/database.py
@@ -6,10 +6,8 @@ class Settings(BaseSettings):
     DATABASE_USERNAME: str
     DATABASE_PASSWORD: str
 
-    model_config = {
-        "env_file": ".env",
-        "env_file_encoding": "utf-8"
-    }
+    class Config:
+        env_file = "../.env"
 
 
 settings = Settings()
